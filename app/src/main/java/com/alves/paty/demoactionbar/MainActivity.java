2bee12627603b43edbce20a1dd7fec071d0a12e7
patyalves17 <Patyptrc2915>
2017-06-05 20:28:31 -0300

    menu

--- app/src/main/java/com/alves/paty/demoactionbar/MainActivity.java
+++ app/src/main/java/com/alves/paty/demoactionbar/MainActivity.java
@@ -2,6 +2,9 @@ package com.alves.paty.demoactionbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item4:
+                Toast.makeText(this, "Item 4", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
